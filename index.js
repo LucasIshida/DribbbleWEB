fdb7e56b890a957955a81401c560365171880b92
Erika <erikahamakami@hotmail.com>
2021-04-12 17:32:29 -0300

    add e iniciando o index.js

---
+++ index.js
@@ -0,0 +1,7 @@
+document.getElementById("email").value = "";
+document.getElementById("password").value = "";
+
+var btn_login = document.querySelector('#botao-login');
+var login_dados = document.querySelector('.dados-login');
+
+
